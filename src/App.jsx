c322fbf47505841e18f8da3bc29a74197a8b7e5e
AaronConnolly <c22410766@mytudublin.ie>
2025-04-20 14:45:12 +0100

    Re-configured the workspace for vite, react, and tailwind to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Experience from './pages/Experience';
-import Navbar from './components/Navbar';
+import './App.css'
 
-export default function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
-    </Router>
-  );
+function App() {
+  return <></>;
+  
 }
+
+export default App
