b3a78a92fef93d8738d52939100457c5dc761bd6
AaronConnolly <c22410766@mytudublin.ie>
2025-04-21 12:06:19 +0100

    Created the home section Initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import './App.css'
-import { LoadingScreen } from './components/LoadingScreen';
-import { Navbar } from './components/Navbar';
-import { MobileMenu } from './components/MobileMenu';
-import "./index.css"
+import React, { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
+import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -11,17 +12,19 @@ function App() {
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {" "}
-      <div className={`min-h-screen transition-opacity duration-700 ${
-        isLoaded ? "opacity-100" : "opacity-0"
-      } bg-black text-gray-100`}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
       >
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        -
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
-  
 }
 
-export default App
+export default App;
